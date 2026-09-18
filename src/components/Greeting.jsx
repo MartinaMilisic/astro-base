174194a0876324a31e8ce39fc17c2234505bf3a3
Martina Milisic <marty@Martinas-MacBook-Pro.local>
2026-09-18 19:11:44 +0200

    adding theme dark + fixing styles

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,7 +9,7 @@ export default function Greeting({messages}) {
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
+      <h2>{greeting}! Thank you for visiting!</h2>
       <button className={styles.button} onClick={() => setGreeting(randomMessage())}>
         New Greeting
       </button>
